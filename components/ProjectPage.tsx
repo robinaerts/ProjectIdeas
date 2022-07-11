e91c026d4c1c6c9964e5010a1bd1af9437bd54df
Robin <nibor.aerts@gmail.com>
2022-07-11 23:02:27 +0200

    styled project page

--- components/ProjectPage.tsx
+++ components/ProjectPage.tsx
@@ -1,64 +1,64 @@
 import { DocumentData } from "firebase/firestore";
-import type { Project } from "./ProjectList"
 
 const ProjectPage = ({project}: DocumentData) => {
-    let difficulty = "Beginner";
+    let difficulty = "Loading...";
     if (project.difficulty == 1) difficulty = "Beginner";
     if (project.difficulty == 2) difficulty = "Intermediate";
     if (project.difficulty == 3) difficulty = "Advanced";
     
     return (
-        <div>
-            <div>
-            <div>
+        <div id="project-page-container">
+            <div id="project-page-left">
+            <div id="project-heading-container">
                 <h2>{project.title}</h2>
                 <p>| {difficulty}</p>
             </div>
-            <div>
+            <div id="description-container">
                 <p>{project.description}</p>
             </div>
             <div>
-                <h3>Constraints</h3>
+                <h3 className="project-page-section-title">Constraints</h3>
                 {project.constraints.map((constraint:string) => {
-                    return <p> - {constraint}</p>
+                    return <p className="project-page-constraint"> - {constraint}</p>
                 })}
             </div>
             <div>
-                <h3>User Stories</h3>
+                <h3 className="project-page-section-title">User Stories</h3>
                 {project.userStories.map((userStory:string) => {
                     return (
-                        <>
+                        <div className="checkbox-container">
                             <input type="checkbox"/>
                             <p>{userStory}</p>
-                        </>
+                        </div>
                     )
                 })}
             </div>
             <div>
-                <h3>Bonus Features</h3>
+                <h3 className="project-page-section-title">Bonus Features</h3>
                 {project.bonusFeatures.map((bonus:string) => {
                     return (
-                        <>
+                        <div className="checkbox-container">
                             <input type="checkbox"/>
                             <p>{bonus}</p>
-                        </>
+                        </div>
                     )
                 })}
             </div>
             </div>
-            <div>
+            <div id="project-page-right">
                 <h3>Useful Links</h3>
                 {project.usefulLinks.map((link:string) => {
                     return (
                         <a href={link}>{link}</a>
                     )
                 })}
-                <h3>Example Projects</h3>
+                <h3 style={{marginTop: "3rem"}}>Example Projects</h3>
                 {project.examples.map((example:string) => {
                     return (
                         <a href={example}>{example}</a>
                     )
                 })}
+                <button id="add-example-btn">ADD YOUR PROJECT</button>
             </div>
         </div>
     )
