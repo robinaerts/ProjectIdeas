5e47fa1d0c31be40b21ec5b7fa1e2d6f579a1ae7
Robin <nibor.aerts@gmail.com>
2022-07-12 09:51:00 +0200

    localstorage when checked

--- components/ProjectPage.tsx
+++ components/ProjectPage.tsx
@@ -1,4 +1,5 @@
 import { DocumentData } from "firebase/firestore";
+import { useEffect, useState } from "react";
 
 const ProjectPage = ({project}: DocumentData) => {
     let difficulty = "Loading...";
@@ -6,12 +7,42 @@ const ProjectPage = ({project}: DocumentData) => {
     if (project.difficulty == 2) difficulty = "Intermediate";
     if (project.difficulty == 3) difficulty = "Advanced";
     
+    const [initialLocalStorage, setInitialLocalStorage] = useState<string[]>([])
+    useEffect(() => {
+        const temp = localStorage.getItem(project.title.toLowerCase()) || "[]";
+        setInitialLocalStorage(JSON.parse(temp));
+    }, [])
+
+    const changeSelect = (e:any) => {
+        const key = project.title.toLowerCase();
+        const id:string = e.target.id;
+        const isChecked = e.target.checked;
+        const prev = localStorage.getItem(key);
+        if (isChecked && prev == undefined) {
+            const newArr = [id]
+            localStorage.setItem(key, JSON.stringify(newArr))
+        } else if (isChecked && prev != undefined) {
+            const arr = JSON.parse(prev);
+            arr.push(id);
+            localStorage.setItem(key, JSON.stringify(arr))
+        } else if ( prev != undefined) {
+            console.log("newArr");
+            const arr = JSON.parse(prev);
+            const newArr = arr.filter((item:string) => {
+                return (item.toLowerCase() != id.toLowerCase());
+            })
+            
+            localStorage.setItem(key, JSON.stringify(newArr))
+        }
+    }
+
     return (
         <div id="project-page-container">
             <div id="project-page-left">
             <div id="project-heading-container">
                 <h2>{project.title}</h2>
                 <p>| {difficulty}</p>
+                {/* {localStorage.getItem(project.title.toLowerCase()) && <button id="finished">Finished!</button>} */}
             </div>
             <div id="description-container">
                 <p>{project.description}</p>
@@ -27,8 +58,8 @@ const ProjectPage = ({project}: DocumentData) => {
                 {project.userStories.map((userStory:string) => {
                     return (
                         <div className="checkbox-container">
-                            <input type="checkbox"/>
-                            <p>{userStory}</p>
+                            <input defaultChecked={initialLocalStorage.includes(userStory)} onChangeCapture={(e) => changeSelect(e)} id={userStory} type="checkbox"/>
+                            <label htmlFor={userStory}>{userStory}</label>
                         </div>
                     )
                 })}
@@ -38,8 +69,8 @@ const ProjectPage = ({project}: DocumentData) => {
                 {project.bonusFeatures.map((bonus:string) => {
                     return (
                         <div className="checkbox-container">
-                            <input type="checkbox"/>
-                            <p>{bonus}</p>
+                            <input defaultChecked={initialLocalStorage.includes(bonus)} onChangeCapture={(e) => changeSelect(e)} style={{background: "green"}} id={bonus} type="checkbox"/>
+                            <label htmlFor={bonus}>{bonus}</label>
                         </div>
                     )
                 })}
